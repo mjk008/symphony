a6df01887becfa918cfd779af7c09a9fea2e6af0
manoj <manoj kumar>
2019-07-10 15:20:08 +0530

    initial commit

---
+++ parallax.js
@@ -0,0 +1,165 @@
+var image = document.getElementsByClassName('symphony-txt u-image u-image-default u-image-1');
+new simpleParallax(image, {
+    delay: .6,
+    orientation: 'down',
+    scale: 1.8,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('img1 u-image u-image-default u-image-2');
+new simpleParallax(image, {
+    delay: .6,
+    orientation: 'down',
+    scale: 1.4,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('u-image u-image-default wlbtep u-image-3');
+new simpleParallax(image, {
+    delay: .6,
+    orientation: 'down',
+    scale: 1.6,
+	overflow: true
+});
+
+//section 1 ends here
+
+var image = document.getElementsByClassName('img2 u-image u-image-default u-image-1');
+new simpleParallax(image, {
+    delay: 3.6,
+    orientation: 'up-right',
+    scale: 1.5,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('u-image u-image-default wirelessbt u-image-2');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'down-right',
+    scale: 2.1,
+	overflow: true
+});
+
+
+
+//section 2 ends here
+
+var image = document.getElementsByClassName('e12 u-image u-image-default u-image-1');
+new simpleParallax(image, {
+    delay: 3.6,
+    orientation: 'up-right',
+    scale: 1.5,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('nband u-image u-image-default u-image-4');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'up-right',
+    scale: 2.1,
+	overflow: true
+});
+
+//section 3 ends here
+
+var image = document.getElementsByClassName('sproof u-image u-image-default u-image-1');
+new simpleParallax(image, {
+    delay: 3.6,
+    orientation: 'up-left',
+    scale: 1.5,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('splash u-image u-image-default u-image-2');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'left',
+    scale: 1.5,
+	overflow: true
+});
+
+//section 4 ends here
+
+var image = document.getElementsByClassName('dual-pairing u-image u-image-default u-image-1');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'down',
+    scale: 1.5,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('img5 u-image u-image-default u-image-2');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'down',
+    scale: 1.5,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('mb2 u-image u-image-default u-image-3');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'up',
+    scale: 2.5,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('mb1 u-image u-image-default u-image-4');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'up',
+    scale: 2.5,
+	overflow: true
+});
+//section 5 ends here
+
+var image = document.getElementsByClassName('img6 u-image u-image-default u-image-1');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'up',
+    scale: 1.4,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('control mv u-image u-image-default u-image-2');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'up',
+    scale: 2.5,
+	overflow: true
+});
+//section 6 ends here
+
+var image = document.getElementsByClassName('img7 u-image u-image-default u-image-1');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'up',
+    scale: 1.4,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('mg');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'left',
+    scale: 2.5,
+	overflow: true
+});
+//section 7 ends here
+
+var image = document.getElementsByClassName('img8 u-image u-image-default u-image-1');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'up',
+    scale: 1.4,
+	overflow: true
+});
+
+var image = document.getElementsByClassName('mgb');
+new simpleParallax(image, {
+    delay: 4.6,
+    orientation: 'up',
+    scale: 2.5,
+	overflow: true
+});
+//section 7 ends here
